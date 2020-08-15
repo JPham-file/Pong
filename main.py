d47d26187ff586d38f5fcb56aaf2113448ea34a1
Johnson Pham <john5onpham@gmail.com>
2020-08-15 17:36:25 -0500

    added objects in game

--- main.py
+++ main.py
@@ -14,6 +14,10 @@ pygame.display.set_caption('Pong')
 
 ball = pygame.Rect(screen_width/2 - 15, screen_height/2 - 15, 30, 30)
 player = pygame.Rect(screen_width - 20, screen_height/2 - 70, 10, 140)
+opponent = pygame.Rect(10, screen_height/2 -70, 10, 140)
+
+bg_color = pygame.Color('grey12')
+light_grey = (200, 200, 200)
 
 running = True 
 while running:
@@ -21,6 +25,13 @@ while running:
         if event.type == pygame.QUIT:
             pygame.quit()
             sys.exit()
+
+    screen.fill(bg_color)
+    pygame.draw.rect(screen, light_grey, player)
+    pygame.draw.rect(screen, light_grey, opponent)
+    #pygame.draw.ellipse(screen, light_grey, ball)
+    pygame.draw.rect(screen, light_grey, ball)
+    pygame.draw.aaline(screen, light_grey, (screen_width/2, 0), (screen_width / 2, screen_height))
 
     # update window
     pygame.display.flip()
