c40fd1482156993104ec594a799539f7b8c38c25
Johnson Pham <john5onpham@gmail.com>
2020-08-15 17:29:24 -0500

    adjusted screen again

--- main.py
+++ main.py
@@ -6,10 +6,14 @@ clock = pygame.time.Clock()
 
 
 # setting up the main window 
-screen_width = 800
-screen_height = 600
+screen_width = 960
+screen_height = 720
 screen = pygame.display.set_mode((screen_width, screen_height))
 pygame.display.set_caption('Pong')
+
+
+ball = pygame.Rect(screen_width/2 - 15, screen_height/2 - 15, 30, 30)
+player = pygame.Rect(screen_width - 20, screen_height/2 - 70, 10, 140)
 
 running = True 
 while running:
@@ -20,4 +24,4 @@ while running:
 
     # update window
     pygame.display.flip()
-    clock.tick(60)
+    clock.tick(60) # frames
